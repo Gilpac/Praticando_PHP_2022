25f7135e6a8e14d18d568fe3e26cafb09799d70e
Gilson <gilpacsebastiao@hotmail.com>
2022-08-27 07:23:53 +0100

    Funções Matemáticas

--- pratica04.php
+++ pratica04.php
@@ -1,10 +1,13 @@
 <?php
-    $valor= 2.3;
+    $numeros=[2,90,3,-20, 10.2];
+
+    $valor= 81.42;
     $tipo_de_dado_alterado= (string) $valor;
     $b= var_dump ($tipo_de_dado_alterado);
     $a= var_dump(is_int($valor));
     $c= var_dump(is_numeric($valor));
-    
+    //A função absolute ou abs() tem a função de transformar um nº negativo em positivo 
+    //A Função rand gera números aleatórios
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +20,13 @@
 </head>
 <body>
     <?php
+      echo "<h2>".pi()."</h2>";
+      echo var_dump($numeros);
+      echo "<h2>  O maior número dessa lista é o nº ".max($numeros)."</h2>";
+      echo "<h2>  O menor número dessa lista é o nº ".min($numeros)."</h2>";
+      echo "<h2>  A raiz quadrada de $valor é : ".sqrt($valor)."</h2>";
+      echo "<h2>  Arredondando o nº $valor, ficaria ". round($valor)."</h2>";
+      echo "<h2>".rand(1,9)."</h2>";
       
     ?>
 </body>
