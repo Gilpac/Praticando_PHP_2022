5003c01bcde80902fd84e3ca45deeac593d23403
Gilson <gilpacsebastiao@hotmail.com>
2022-08-27 06:43:00 +0100

    Manipulação de Números

---
+++ pratica04.php
@@ -0,0 +1,23 @@
+<?php
+    $valor= 2.3;
+    $tipo_de_dado_alterado= (string) $valor;
+    $b= var_dump ($tipo_de_dado_alterado);
+    $a= var_dump(is_int($valor));
+    $c= var_dump(is_numeric($valor));
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulação de números</title>
+</head>
+<body>
+    <?php
+      
+    ?>
+</body>
+</html>
